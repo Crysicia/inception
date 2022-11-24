c1e6511c81929d8c40b5ad096c3021650091a092
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-11-24 19:59:48 +0100

    Add mariadb and initialize WP

---
+++ srcs/wordpress/config/run.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+if [ -f /var/www/wordpress/wp-config.php ]
+then
+	echo "Wordpress already downloaded"
+else
+	cd /var/www/wordpress
+	wp core download --allow-root
+	wp config create --dbhost=mariadb:3306 --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
+	wp core install --url=lpassera.42.fr --title="lpassera's ft_server" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD --allow-root
+fi
+
+/usr/sbin/php-fpm7.3 -F
