65f39dd7f09c209f9ed36d15cbee6c25966b7079
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-12-14 13:44:43 +0100

    Add volume drivers

--- srcs/wordpress/config/run.sh
+++ srcs/wordpress/config/run.sh
@@ -2,13 +2,14 @@
 
 if [ -f /var/www/wordpress/wp-config.php ]
 then
-	echo "Wordpress already downloaded"
+	echo "[run.sh] Wordpress already downloaded"
 else
 	cd /var/www/wordpress
 	wp core download --allow-root
 	wp config create --dbhost=mariadb:3306 --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
 	wp core install --url=lpassera.42.fr --title="lpassera's ft_server" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD --allow-root
+	chown www-data:www-data /var/www/wordpress
 fi
 
 /usr/sbin/php-fpm7.3 -F
