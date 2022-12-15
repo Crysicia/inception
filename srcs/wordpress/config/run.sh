930cf94a99cc2e54e587878b86bb1bb79ba12ce4
Crysicia <7459685+Crysicia@users.noreply.github.com>
2022-12-15 15:06:09 +0100

    Fix TLS version

--- srcs/wordpress/config/run.sh
+++ srcs/wordpress/config/run.sh
@@ -7,7 +7,7 @@ else
 	cd /var/www/wordpress
 	wp core download --allow-root
 	wp config create --dbhost=mariadb:3306 --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
-	wp core install --url=lpassera.42.fr --title="lpassera's ft_server" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
+	wp core install --url=lpassera.42.fr --title="lpassera's inception" --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PASSWORD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 	wp user create $WORDPRESS_USER $WORDPRESS_EMAIL --user_pass=$WORDPRESS_PASSWORD --allow-root
 	chown -R www-data:www-data /var/www/wordpress
 fi
